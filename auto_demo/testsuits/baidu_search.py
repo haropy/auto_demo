e6f8fc2c9cd6f8ecfd7de4a1999cdea9924132e3
kongxi <kongxi@aiyunxiao.com>
2019-08-12 15:43:44 +0800

    Python+Selenium框架设计篇之6-一个类文件多个测试方法情况下测试固件的写法

--- auto_demo/testsuits/baidu_search.py
+++ auto_demo/testsuits/baidu_search.py
@@ -7,20 +7,21 @@ from auto_demo.pageobjects.baidu_homepage import HomePage
 
 class BaiduSearch(unittest.TestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         """
         测试固件的setUp()的代码，主要是测试的前提准备工作
         :return:
         """
-        browse = BrowserEngine(self)
-        self.driver = browse.open_browser(self)
-
-    def tearDown(self):
+        browse = BrowserEngine(cls)
+        cls.driver = browse.open_browser(cls)
+    @classmethod
+    def tearDownClass(cls):
         """
         测试结束后的操作，这里基本上都是关闭浏览器
         :return:
         """
-        self.driver.quit()
+        cls.driver.quit()
 
     def test_baidu_search(self):
         """
