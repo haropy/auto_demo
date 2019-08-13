74e6c1971492dcdd8851f11a9eb90a5e83d51885
kongxi <kongxi@aiyunxiao.com>
2019-08-13 09:34:01 +0800

    Python+Selenium框架设计篇之9-unittest执行脚本方法之makeSuite()

--- auto_demo/testsuits/TestRunner.py
+++ auto_demo/testsuits/TestRunner.py
@@ -3,10 +3,11 @@ import unittest
 from auto_demo.testsuits.test_baidu_search import BaiduSearch
 from auto_demo.testsuits.test_get_page_title import GetPageTtitle
 
-suite = unittest.TestSuite()
-suite.addTest(BaiduSearch('test_baidu_search'))
-suite.addTest(BaiduSearch('test_search2'))
-suite.addTest(GetPageTtitle('test_get_title'))
+suite = unittest.TestSuite(unittest.makeSuite(BaiduSearch))
+# suite(unittest.makeSuite(GetPageTtitle))
+# suite.addTest(BaiduSearch('test_baidu_search'))
+# suite.addTest(BaiduSearch('test_search2'))
+# suite.addTest(GetPageTtitle('test_get_title'))
 if __name__ == '__main__':
     # 执行用例
     runner = unittest.TextTestRunner()
