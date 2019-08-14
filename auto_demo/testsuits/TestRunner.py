cb3cd001572080b3175c7866fe09c0adf67450b9
kongxi <kongxi@aiyunxiao.com>
2019-08-14 19:03:07 +0800

    Python+Selenium框架设计篇之11-自动化测试报告的生成

--- auto_demo/testsuits/TestRunner.py
+++ auto_demo/testsuits/TestRunner.py
@@ -1,16 +1,23 @@
-# coding:utf-8
+# coding=utf-8
+import HTMLTestRunner
+import os
 import unittest
-import auto_demo.testsuits
-from auto_demo.testsuits.test_baidu_search import BaiduSearch
-from auto_demo.testsuits.test_get_page_title import GetPageTtitle
+import time
 
-suite = unittest.TestLoader().discover("testsuits")
-# suite = unittest.TestSuite(unittest.makeSuite(BaiduSearch))
-# suite(unittest.makeSuite(GetPageTtitle))
-# suite.addTest(BaiduSearch('test_baidu_search'))
-# suite.addTest(BaiduSearch('test_search2'))
-# suite.addTest(GetPageTtitle('test_get_title'))
+# 设置报告文件保存路径
+report_path = os.path.dirname(os.path.abspath('.')) + '/test_report/'
+# 获取系统当前时间
+now = time.strftime("%Y-%m-%d-%H_%M_%S", time.localtime(time.time()))
+
+# 设置报告名称格式
+HtmlFile = report_path + now + "HTMLtemplate.html"
+fp = file(HtmlFile, "wb")
+
+# 构建suite
+suite = unittest.TestLoader().discover("auto_demo.testsuits")
+
 if __name__ == '__main__':
-    # 执行用例
-    runner = unittest.TextTestRunner()
+    # 初始化一个HTMLTestRunner实例对象，用来生成报告
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title=u"某某项目测试报告", description=u"用例测试情况")
+    # 开始执行测试套件
     runner.run(suite)
